7da3a7aea3cf18bf5a98b49b082ec14e56e30d2f
JanaDolejsi <jana.dolejsi.gr@gmail.com>
2024-04-05 14:43:47 +0200

    pokus o JS, nefunguje

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('funguju');
+
+
+function changeColor(color) {
+  var productImage = document.querySelector("#productImage");
+  if (color === 'white') {
+    productImage.style.filter = 'grayscale(0%)'; 
+  } else if (color === 'black') {
+    productImage.style.filter = 'grayscale(100%)'; 
+  } 
+}
+
+var buttons = document.querySelectorAll('.color-button');
+buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    var color = this.getAttribute('data-color');
+    changeColor(color);
+  });
+});
