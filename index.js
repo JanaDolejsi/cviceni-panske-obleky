8edad35a502560cd53d1a7276ecf24c96a9d06b8
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-04-06 00:54:29 +0200

    přidány varianty bez array a loopu a s nimi, zakomentování 2 variant

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-console.log('funguju');
+console.log("funguju");
 
-
+/*
+// Janči varianta
 function changeColor(color) {
   var productImage = document.querySelector("#productImage");
   if (color === 'white') {
@@ -17,3 +18,72 @@ buttons.forEach(function(button) {
     changeColor(color);
   });
 });
+
+//Varianta bez znalosti array a for loop, není moc DRY, ale funguje
+// element chosen
+const shirt = document.getElementById("product-image");
+const whiteBtn = document.getElementById("whiteBtn");
+const blackBtn = document.getElementById("blackBtn");
+const redBtn = document.getElementById("redBtn");
+const blueBtn = document.getElementById("blueBtn");
+const yellowBtn = document.getElementById("yellowBtn");
+const greenBtn = document.getElementById("greenBtn");
+
+// variables declaration
+let chosenColor;
+
+whiteBtn.addEventListener("click", function () {
+  const color = this.getAttribute("data-color");
+  shirt.style = `fill: ${color}`;
+  chosenColor = color;
+});
+
+blackBtn.addEventListener("click", function () {
+  const color = this.getAttribute("data-color");
+  shirt.style = `fill: ${color}`;
+  chosenColor = color;
+});
+
+redBtn.addEventListener("click", function () {
+  const color = this.getAttribute("data-color");
+  shirt.style = `fill: ${color}`;
+  chosenColor = color;
+});
+
+blueBtn.addEventListener("click", function () {
+  const color = this.getAttribute("data-color");
+  shirt.style = `fill: ${color}`;
+  chosenColor = color;
+});
+
+yellowBtn.addEventListener("click", function () {
+  const color = this.getAttribute("data-color");
+  shirt.style = `fill: ${color}`;
+  chosenColor = color;
+});
+
+greenBtn.addEventListener("click", function () {
+  const color = this.getAttribute("data-color");
+  shirt.style = `fill: ${color}`;
+  chosenColor = color;
+});
+*/
+
+// Varianta se znalostí array a for loop
+// element chosen
+const shirt = document.getElementById("product-image");
+
+// variables declaration
+let chosenColor;
+
+// pole barev
+const colorPool = ["white", "black", "red", "blue", "yellow", "green"];
+
+for (let i = 0; i < colorPool.length; i++) {
+  const clickedBtn = document.getElementById(`${colorPool[i]}Btn`);
+  clickedBtn.addEventListener("click", function () {
+    const color = this.getAttribute("data-color");
+    shirt.style = `fill: ${color}`;
+    chosenColor = color;
+  });
+}
